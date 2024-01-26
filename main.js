24318453aef9b522d48975e8f223e4b69421d2b7
tuumama <ronybilletespc@gmail.com>
2024-01-25 21:26:32 -0600

    nuevas cosas y foto

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".text", {
     strings: ["Brandon", "Ronald", "Cristian", "Yarim", "Luis A", "Antonio", "Yael", "Alfred", "Ángel", "Rebeca", "Dariana", "Alexa", "Carlos", "Jorge", "Profe Misael"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 
 })
